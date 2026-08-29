e6316f63ab6b15f03e5514a414663eda6dce1cd5
MareTheGeek <subomimare@gmail.com>
2026-08-29 03:17:29 -0400

    'fonts'

--- app/(tabs)/Insights.tsx
+++ app/(tabs)/Insights.tsx
@@ -1,5 +1,5 @@
 import { styled } from 'nativewind'
-import { Text, View } from 'react-native'
+import { Text } from 'react-native'
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context'
 
 const SafeAreaView = styled(RNSafeAreaView)
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,32 @@
 import '@/global.css'
 import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { styled } from 'nativewind'
+import { Text } from 'react-native'
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context'
-import {styled} from 'nativewind'
 
 const SafeAreaView = styled(RNSafeAreaView)
 
 export default function App() {
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text className='text-xl font-bold text-success'>
-        Welcome to Nativewind!
-      </Text>
+      <Text className='text-5xl font-sans-extrabold'>Home</Text>
       <Link
         href='/Onboarding'
-        className='mt-4 rounded bg-primary text-white p-4'
+        className='mt-4 font-sans-bold rounded bg-primary text-white p-4'
       >
         Onboarding
       </Link>
       <Link
         href='/(auth)/SignIn'
-        className='mt-4 rounded bg-primary text-white p-4'
+        className='mt-4 font-sans-bold rounded bg-primary text-white p-4'
       >
         Login
       </Link>
       <Link
         href='/(auth)/SignUp'
-        className='mt-4 rounded bg-primary text-white p-4'
+        className='mt-4 font-sans-bold rounded bg-primary text-white p-4'
       >
         Sign up
-      </Link>
-      <Link href='/subscriptions/Spotify'>Spotify Subscription</Link>
-      <Link
-        href={{
-          pathname: '/subscriptions/[id]',
-          params: { id: 'claude' },
-        }}
-      >
-        Claude Max Subscription
       </Link>
     </SafeAreaView>
   )
